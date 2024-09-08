1ae72daa3e214f040aaa5197e5cdf749bc7906ef
franosores <francisca@montblanc.ai>
2024-09-08 18:50:41 -0300

    [INIT] main file

---
+++ main.py
@@ -0,0 +1,8 @@
+from gpt_fragments.fragment_processor import FragmentProcessor
+
+def main():
+    processor = FragmentProcessor()
+    processor.process_fragments('data/input.jsonl', 'output/processed_output.jsonl')
+
+if __name__ == "__main__":
+    main()
