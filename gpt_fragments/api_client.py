37e0bae666b1ded40b2c109c06915139a258eb17
franosores <francisca@montblanc.ai>
2024-09-09 18:05:10 -0300

    [UPDATE] improve prompt generation and cleaned response

--- gpt_fragments/api_client.py
+++ gpt_fragments/api_client.py
@@ -1,5 +1,6 @@
 from os import environ
 from pathlib import Path
+import re
 
 from dotenv import load_dotenv
 from openai import OpenAI
@@ -13,16 +14,20 @@ class GPTClient:
     def __init__(self):
         self.client = OpenAI(api_key=OPENAI_TOKEN)
 
-    def generate_summary_and_tags(self, content):
+    def generate_summary_and_tags(self, content, prompt=None):
         response = self.client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[
-                {"role": "system", "content": "Resume el siguiente contenido y genera tags:."},
-                {"role": "user", "content": content},
+                {"role": "system",
+                 "content": prompt},
+                {"role": "user",
+                 "content": f"Para el siguiente contenido, quiero que generes una respuesta en este formato específico:\n\n**Título:** [Título del artículo]\n\n**Resumen:** [Resumen del contenido]\n\n**Tags:** [#Tag1 #Tag2 ...]\n\nContenido:\n{content}"},
             ],
             max_tokens=1000,
         )
-        summary = response.choices[0].message.content
-        # print(f"Summary: {summary}")
-        tags = ["Api", "Tickets"]
-        return summary, tags
+        response = response.choices[0].message.content
+        title = re.search(r'\*\*Título:\*\*(.*?)\n', response).group(1).strip()
+        summary = re.search(r'\*\*Resumen:\*\*(.*?)\n\n', response, re.DOTALL).group(1).strip()
+        tags = re.search(r'\*\*Tags:\*\*(.*?)$', response, re.DOTALL).group(1).strip().split()
+
+        return title, summary, tags
