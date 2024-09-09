0eeb46ae2cc6dada9d7cde71ea79a15f18b00aae
franosores <francisca@montblanc.ai>
2024-09-08 23:58:30 -0300

    [FIX] use original file name

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from gpt_fragments.fragment_processor import FragmentProcessor
 
 def main():
     processor = FragmentProcessor()
-    processor.process_fragments('data/input.jsonl', 'output/processed_output.jsonl')
+    processor.process_fragments('data/adereso_cda.jsonl', 'output/processed_output.jsonl')
 
 if __name__ == "__main__":
     main()
