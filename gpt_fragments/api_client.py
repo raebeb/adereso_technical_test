92079e38bedb4df9dc8bb67296ce0e4e4c890733
franosores <francisca@montblanc.ai>
2024-09-08 23:58:00 -0300

    [UPDATE] update method to use the latest version of openai client

--- gpt_fragments/api_client.py
+++ gpt_fragments/api_client.py
@@ -1,17 +1,28 @@
-import openai
+from os import environ
+from pathlib import Path
 
+from dotenv import load_dotenv
+from openai import OpenAI
 
+dotenv_path = Path('enviroment/.env')
+load_dotenv(dotenv_path)
+
+OPENAI_TOKEN = environ.get('OPENAI_TOKEN')
 
 class GPTClient:
     def __init__(self):
-        openai.api_key = 'your-api-key-here'
+        self.client = OpenAI(api_key=OPENAI_TOKEN)
 
     def generate_summary_and_tags(self, content):
-        response = openai.Completion.create(
+        response = self.client.chat.completions.create(
             model="gpt-4o-mini",
-            prompt=f"Resume el siguiente contenido y genera tags:\n\n{content}", #TODO: initial version, remove this when the prompt is ready
-            max_tokens=1000
+            messages=[
+                {"role": "system", "content": "Resume el siguiente contenido y genera tags:."},
+                {"role": "user", "content": content},
+            ],
+            max_tokens=1000,
         )
-        summary = response['choices']
+        summary = response.choices[0].message.content
+        # print(f"Summary: {summary}")
         tags = ["Api", "Tickets"]
         return summary, tags
